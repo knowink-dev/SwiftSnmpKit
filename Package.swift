0e3c9b13e766c83af71ec05aca740d560996d8f3
Adam Hitt <adam.hitt@knowink.com>
2023-12-06 11:36:59 -0600

    Updated package to support iOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,6 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftSnmpKit",
     platforms: [
+        .iOS(.v16),
         .macOS(.v10_15)
     ],
     products: [
